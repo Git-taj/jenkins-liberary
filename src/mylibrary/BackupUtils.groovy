50a595aa15741e2893359808dd79951dfd939e50
Git-taj <110991920+Git-taj@users.noreply.github.com>
2023-07-05 16:48:59 +0530

    Update and rename MyScript.groovy to BackupUtils.groovy

---
+++ src/mylibrary/BackupUtils.groovy
@@ -0,0 +1,18 @@
+package mylibrary
+
+import org.apache.commons.io.FileUtils
+
+class BackupUtils {
+    static void performBackup() {
+        def srcDir = new File('/var/lib/jenkins')
+        def destDir = new File('/opt/backup1')
+
+        try {
+            FileUtils.copyDirectory(srcDir, destDir)
+            println "Backup successful. Files copied from ${srcDir} to ${destDir}."
+        } catch (Exception e) {
+            println "Error occurred during backup: ${e.message}"
+        }
+    }
+}
+
